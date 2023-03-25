0822d955d43fee8e1bb1764b61831c8602f9618f
Roz <roz@rjmunhoz.me>
2023-03-25 12:59:43 -0300

    !feat: new composer logic
    - The `execute` function now resolves to the final context object
    - New `before` and `after` functions
    
    BREAKING CHANGES: The `execute` function now resolves to the final context object. You are now required to pass the context to `next`

--- mod.ts
+++ mod.ts
@@ -1,95 +1,131 @@
 type MaybePromise<T> = Promise<T> | T;
 type MaybeArray<T> = Array<T> | T;
 
-type NextFunction = (err?: unknown) => MaybePromise<void>;
-export type MiddlewareFn<C> = (ctx: C, next: NextFunction) => void;
-export type MiddlewareObj<C> = {
-  middleware: () => MiddlewareFn<C>;
+type NextFunction<TContext> = (ctx: TContext) => void;
+type MiddlewareFn<TContext> = (ctx: TContext, next: NextFunction<TContext>) => void;
+type MiddlewareObj<TContext> = {
+  middleware(): MiddlewareFn<TContext>;
 };
-export type Middleware<C> = MiddlewareFn<C> | MiddlewareObj<C>;
+type Middleware<TContext> = MiddlewareFn<TContext> | MiddlewareObj<TContext>;
 
-function concat<C>(
-  first: MiddlewareFn<C>,
-  andThen: MiddlewareFn<C>,
-) {
-  return (ctx: C, next: NextFunction) => {
+export function concat<TContext>(first: MiddlewareFn<TContext>, andThen: MiddlewareFn<TContext>) {
+  return (ctx: TContext, next: NextFunction<TContext>) => {
     let nextCalled = false;
-    return first(ctx, (err) => {
-      if (err) throw err;
-      if (nextCalled) throw new Error("next already called");
+    return first(ctx, async (newCtx) => {
+      if (nextCalled) throw new Error("next already called!");
       nextCalled = true;
-      return andThen(ctx, next);
+      await andThen(newCtx, next);
+      return newCtx;
     });
   };
 }
 
-function flatten<C>(mw: Middleware<C>): MiddlewareFn<C> {
-  return typeof mw === "function" ? mw : (ctx, next) => mw.middleware()(ctx, next);
+export function flatten<TContext>(middleware: Middleware<TContext>) {
+  return typeof middleware === "function" ? middleware : middleware.middleware();
 }
 
-function run<C>(middleware: Middleware<C>, ctx: C, next?: NextFunction) {
-  return flatten(middleware)(ctx, () => next ? next() : Promise.resolve());
+export function run<TContext>(ctx: TContext, middleware: MiddlewareFn<TContext>) {
+  return new Promise<TContext>((resolve, reject) => {
+    try {
+      middleware(ctx, resolve);
+    } catch (err) {
+      reject(err);
+    }
+  });
 }
 
-export class Composer<C> {
-  private handler: MiddlewareFn<C>;
+const pass = <TContext>(ctx: TContext, next: NextFunction<TContext>) => next(ctx);
 
-  constructor(...middleware: Array<Middleware<C>>) {
-    this.handler = middleware.length ? middleware.map(flatten).reduce(concat) : (_, next) => {
-      return next();
-    };
+export class Composer<TContext> {
+  private handler: MiddlewareFn<TContext>;
+
+  constructor(...middleware: Middleware<TContext>[]) {
+    this.handler = middleware.length ? middleware.map(flatten).reduce(concat) : pass;
   }
 
   middleware() {
     return this.handler;
   }
 
-  use(...middleware: Array<Middleware<C>>) {
+  use(...middleware: Middleware<TContext>[]) {
     const composer = new Composer(...middleware);
-    this.handler = concat(this.handler, flatten(composer));
+    this.handler = concat(this.handler, composer.middleware());
     return composer;
   }
 
-  fork(...middleware: Array<Middleware<C>>) {
-    const composer = new Composer(...middleware);
-    this.use((ctx, next) => Promise.all([next(), run(composer, ctx)]));
+  before(...middlewares: Middleware<TContext>[]) {
+    const composer = new Composer<TContext>(...middlewares);
+    this.handler = concat(composer.middleware(), this.handler);
     return composer;
   }
 
-  lazy(middlewareFactory: (ctx: C) => MaybePromise<MaybeArray<Middleware<C>>>) {
+  after(deffered: (ctx: TContext) => void) {
+    this.use(async (ctx, next) => {
+      await next(ctx);
+      deffered(ctx);
+    });
+  }
+
+  fork(...middleware: Array<Middleware<TContext>>) {
+    const composer = new Composer(...middleware);
+    this.use((ctx, next) => Promise.all([next(ctx), run(ctx, composer.middleware())]));
+    return composer;
+  }
+
+  lazy(middlewareFactory: (ctx: TContext) => MaybePromise<MaybeArray<Middleware<TContext>>>) {
     return this.use(async (ctx, next) => {
       const middleware = await middlewareFactory(ctx);
       const arr = Array.isArray(middleware) ? middleware : [middleware];
-      return run(new Composer(...arr), ctx, next);
+      return next(await run(ctx, new Composer(...arr).middleware()));
     });
   }
 
   branch(
-    predicate: (ctx: C) => MaybePromise<boolean>,
-    trueMiddleware: MaybeArray<Middleware<C>>,
-    falseMiddleware: MaybeArray<Middleware<C>>,
+    predicate: (ctx: TContext) => MaybePromise<boolean>,
+    trueMiddleware: MaybeArray<Middleware<TContext>>,
+    falseMiddleware: MaybeArray<Middleware<TContext>>,
   ) {
     return this.lazy(async (ctx) => (await predicate(ctx)) ? trueMiddleware : falseMiddleware);
   }
 
-  filter<D extends C>(
-    predicate: (ctx: C) => ctx is D,
+  filter<D extends TContext>(
+    predicate: (ctx: TContext) => ctx is D,
     ...middleware: Array<Middleware<D>>
   ): Composer<D>;
   filter(
-    predicate: (ctx: C) => MaybePromise<boolean>,
-    ...middleware: Array<Middleware<C>>
+    predicate: (ctx: TContext) => MaybePromise<boolean>,
+    ...middleware: Array<Middleware<TContext>>
   ): this;
   filter(
-    predicate: (ctx: C) => MaybePromise<boolean>,
-    ...middleware: Array<Middleware<C>>
+    predicate: (ctx: TContext) => MaybePromise<boolean>,
+    ...middleware: Array<Middleware<TContext>>
   ) {
     const composer = new Composer(...middleware);
-    this.branch(predicate, composer, (_, next) => next());
+    this.branch(predicate, composer, (_, next) => next(_));
     return composer;
   }
 
-  execute(ctx: C, leaf?: NextFunction) {
-    return run(this, ctx, leaf);
+  execute(ctx: TContext) {
+    return run(ctx, this.handler);
   }
 }
+
+type Context = {
+  start: number;
+};
+
+const composer = new Composer<Context>();
+composer.before((ctx, next) => {
+  console.log("composer before");
+  next({ ...ctx, start: Date.now() });
+});
+composer.after((ctx) => console.log(`Took ${Date.now() - ctx.start} ms`));
+
+composer.use(async (ctx, next) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return next(ctx);
+});
+
+composer.execute({
+  start: 0,
+}).then(console.log);
